c03338a6094bb44fd1f69ab3de9881682c1546ac
jhiy2004 <jhiy2004@gmail.com>
2026-03-24 18:30:49 -0300

    feat: adicionar follow e last

--- packages/core-lib/include/analisador_sintatico.h
+++ packages/core-lib/include/analisador_sintatico.h
@@ -1,32 +1,117 @@
 #pragma once
 
+#include "compilador_lalg.h"
+#include <optional>
 #include <string>
+#include <unordered_set>
 #include <vector>
 #include <memory>
 
 class Elemento {
 public:
-    std::string nome;
-
-    Elemento();
-    Elemento(const std::string& n);
-    virtual ~Elemento();
+    virtual std::string get_nome() const = 0;
+    virtual ~Elemento() = default;
 };
+
+class NaoTerminal;
+class Terminal;
+
+class Regra {
+public:
+    Regra(const std::vector<std::shared_ptr<Elemento>>& elems) : elementos(elems) {}
+    
+    std::optional<std::shared_ptr<NaoTerminal>> get_next_non_terminal(std::shared_ptr<Elemento> elem) const;
+    std::optional<std::shared_ptr<Terminal>> get_next_terminal(std::shared_ptr<Elemento> elem) const;
+
+    bool has_elem(std::shared_ptr<Elemento> elem) const;
+
+    std::shared_ptr<Elemento> get_head() const {
+        return elementos.front();
+    }
+
+    std::shared_ptr<Elemento> get_tail() const {
+        return elementos.back();
+    }
+
+private:
+    std::vector<std::shared_ptr<Elemento>> elementos;
+};
+
 
 class Terminal : public Elemento {
 public:
-    int token;
-
     Terminal();
-    Terminal(const std::string& n, int t);
+    Terminal(const std::string& n, TokenType t);
     ~Terminal();
+
+    virtual std::string get_nome() const override{
+        return nome;
+    };
+private:
+    TokenType token;
+    std::string nome;
 };
 
 class NaoTerminal : public Elemento {
 public:
-    std::vector<std::vector<std::shared_ptr<Elemento>>> producoes;
+    std::vector<Regra> producoes;
 
     NaoTerminal();
-    NaoTerminal(const std::string& n);
+    NaoTerminal(const std::string& n, bool s = false);
     ~NaoTerminal();
+
+    std::unordered_set<std::shared_ptr<Elemento>> get_head() const;
+
+    std::unordered_set<std::shared_ptr<Elemento>> get_last() const {
+        std::unordered_set<std::shared_ptr<Elemento>> res;
+        for (const auto r : producoes) {
+            res.insert(r.get_tail());
+        }
+
+        return res;
+    }
+
+    std::unordered_set<std::shared_ptr<Elemento>> get_firsts() const {
+        return firsts;
+    }
+
+    std::unordered_set<std::shared_ptr<Elemento>> get_follows() const {
+        return follows;
+    }
+
+    bool has_elem(const std::shared_ptr<Elemento> elem) const;
+
+    void add_regra(const Regra& r) {
+        producoes.push_back(r);
+    }
+
+    std::vector<Regra> get_regras() const {
+        return producoes;
+    }
+
+    bool isStart() const;
+
+    virtual std::string get_nome() const override{
+        return nome;
+    };
+private:
+    bool start;
+    std::string nome;
+    std::unordered_set<std::shared_ptr<Elemento>> firsts;
+    std::unordered_set<std::shared_ptr<Elemento>> follows;
+};
+
+
+class GrammarLALG {
+public:
+    GrammarLALG();
+    bool compile();
+private:
+    void first(std::shared_ptr<NaoTerminal> ptr);
+    void follow(std::shared_ptr<NaoTerminal> ptr);
+    std::unordered_set<std::shared_ptr<Elemento>> head(std::shared_ptr<NaoTerminal> ptr);
+    std::unordered_set<std::shared_ptr<Elemento>> last(std::shared_ptr<NaoTerminal> ptr);
+
+    static std::unordered_map<std::string, std::shared_ptr<Terminal>> terminals;
+    static std::unordered_map<std::string, std::shared_ptr<NaoTerminal>> non_terminals;
 };
--- packages/core-lib/src/analisador_sintatico.cpp
+++ packages/core-lib/src/analisador_sintatico.cpp
@@ -1,19 +1,163 @@
 #include "analisador_sintatico.h"
-
-Elemento::Elemento() {}
-
-Elemento::Elemento(const std::string& n) : nome(n) {}
-
-Elemento::~Elemento() {}
+#include <memory>
 
 Terminal::Terminal() {}
-
-Terminal::Terminal(const std::string& n, int t) : Elemento(n), token(t) {}
-
+Terminal::Terminal(const std::string& n, TokenType t) : nome(n), token(t) {}
 Terminal::~Terminal() {}
 
 NaoTerminal::NaoTerminal() {}
-
-NaoTerminal::NaoTerminal(const std::string& n) : Elemento(n) {}
-
+NaoTerminal::NaoTerminal(const std::string& n, bool s) : nome(n), start(s) {};
 NaoTerminal::~NaoTerminal() {}
+
+bool NaoTerminal::has_elem(std::shared_ptr<Elemento> elem) const {
+    for (const Regra& regra : producoes) {
+        if (regra.has_elem(elem)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool NaoTerminal::isStart() const{
+    return start;
+}
+
+std::optional<std::shared_ptr<NaoTerminal>> Regra::get_next_non_terminal(std::shared_ptr<Elemento> elem) const {
+    int res = -1;
+    for (int i=0; i < elementos.size(); i++) {
+        if (elementos[i] == elem) {
+            res = i; 
+            break;
+        }
+    }
+
+    if (res >= 0 && res+1 < elementos.size()){
+        auto next = elementos[res + 1];
+        if (auto next_nt = std::dynamic_pointer_cast<NaoTerminal>(next); next_nt != nullptr){
+            return next_nt;
+        }
+    }
+    return std::nullopt;
+}
+
+std::optional<std::shared_ptr<Terminal>> Regra::get_next_terminal(std::shared_ptr<Elemento> elem) const {
+    int res = -1;
+    for (int i=0; i < elementos.size(); i++) {
+        if (elementos[i] == elem) {
+            res = i; 
+            break;
+        }
+    }
+
+    if (res >= 0 && res+1 < elementos.size()){
+        auto next = elementos[res + 1];
+        if (auto next_t = std::dynamic_pointer_cast<Terminal>(next); next_t != nullptr){
+            return next_t;
+        }
+    }
+    return std::nullopt;
+}
+
+
+bool Regra::has_elem(std::shared_ptr<Elemento> elem) const {
+    for (const auto e : elementos) {
+        if (elem == e) {
+            return true;
+        }
+    }
+    return false;
+}
+
+std::unordered_map<std::string, std::shared_ptr<Terminal>> GrammarLALG::terminals = {
+    {"if", std::make_shared<Terminal>("if", TokenType::IfWord)},
+    {"id", std::make_shared<Terminal>("id", TokenType::Id)},
+    {"$", std::make_shared<Terminal>("$", TokenType::UNK)},
+    {"epsilon", std::make_shared<Terminal>("epsilon", TokenType::UNK)},
+};
+
+std::unordered_map<std::string, std::shared_ptr<NaoTerminal>> GrammarLALG::non_terminals = {
+    {"S", std::make_shared<NaoTerminal>("S", true)},
+    {"A", std::make_shared<NaoTerminal>("A")},
+    {"B", std::make_shared<NaoTerminal>("B")},
+};
+
+GrammarLALG::GrammarLALG() {
+    non_terminals["S"]->add_regra(Regra({ non_terminals["A"], terminals["if"]}));
+    non_terminals["S"]->add_regra(Regra({ terminals["id"] }));
+
+    non_terminals["A"]->add_regra(Regra({ non_terminals["B"], terminals["if"]}));
+    non_terminals["A"]->add_regra(Regra({ terminals["if"] }));
+}
+
+bool GrammarLALG::compile() {
+    //Primeiro devem ser encontrados todos os firsts de todos os nao-terminais
+    for (const auto& pair : non_terminals) {
+        auto nt_ptr = pair.second;
+        first(nt_ptr);
+    }
+
+    //Tendo todos os first é possível encontrar todos os follows
+    for (const auto& pair : non_terminals) {
+        auto nt_ptr = pair.second;
+        follow(nt_ptr);
+    }
+
+    return false;
+}
+
+void GrammarLALG::first(std::shared_ptr<NaoTerminal> ptr) {
+    return; 
+}
+
+void GrammarLALG::follow(std::shared_ptr<NaoTerminal> ptr) {
+    std::unordered_set<std::shared_ptr<Elemento>> follows;
+    std::unordered_set<std::shared_ptr<NaoTerminal>> to_analyze;
+
+    if (ptr->isStart()) {
+        follows.insert(terminals["$"]);
+    }
+
+    for (const auto& pair : non_terminals) {
+        auto nt_ptr = pair.second;
+
+        auto regras = nt_ptr->get_regras();
+        // essas funcs get assumem que tera apenas um NaoTerminal de um tipo por regra.
+        // Aceita. Exemplo: S->Aa
+        // Nao Aceita. Exemplo: S->AbAa
+
+        for (const Regra& r : regras) {
+            auto next_nt = r.get_next_non_terminal(ptr);
+            if (next_nt.has_value()) {
+                to_analyze.insert(next_nt.value());
+            } else if (r.has_elem(ptr)){
+                to_analyze.insert(nt_ptr);
+            }
+
+            auto next_t = r.get_next_terminal(ptr);
+            if (next_t.has_value()) {
+                follows.insert(next_t.value());
+            }
+        }
+    }
+
+    for (const auto elem : to_analyze) {
+        if (elem->has_elem(terminals["epsilon"])) {
+            for (const auto elem : elem->get_follows()) {
+                follows.insert(elem);
+            }
+        } else {
+            for (const auto elem : elem->get_firsts()) {
+                follows.insert(elem);
+            }
+        }
+    }
+}
+
+std::unordered_set<std::shared_ptr<Elemento>> GrammarLALG::head(std::shared_ptr<NaoTerminal> ptr) {
+    return ptr->get_head();
+}
+
+std::unordered_set<std::shared_ptr<Elemento>> GrammarLALG::last(std::shared_ptr<NaoTerminal> ptr) {
+    return ptr->get_last();
+}
