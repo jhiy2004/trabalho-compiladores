626b9f025ed7b58e360f01f26a2a35361205fff2
jhiy2004 <jhiy2004@gmail.com>
2026-07-31 18:06:43 -0300

    feat: add all mepa commands (mepa_example_2.lar working)

--- packages/core-lib/include/mepa.h
+++ packages/core-lib/include/mepa.h
@@ -7,10 +7,176 @@
 #include <vector>
 #include <cstddef>
 #include <optional>
+#include <queue>
+#include <string>
+#include <sstream>
+#include <mutex>
+#include <condition_variable>
+
+template <typename T>
+class ConcurrentQueue {
+public:
+    void push(const T& value) {
+        {
+            std::lock_guard lock(mutex);
+            queue.push(value);
+        }
+        cv.notify_one();
+    }
+
+    T pop() {
+        std::unique_lock lock(mutex);
+
+        cv.wait(lock, [&] {
+            return !queue.empty();
+        });
+
+        T value = std::move(queue.front());
+        queue.pop();
+        return value;
+    }
+
+    void wait_until_size(std::size_t n) {
+        std::unique_lock lock(mutex);
+
+        cv.wait(lock, [&] {
+            return queue.size() >= n;
+        });
+    }
+
+private:
+    std::queue<T> queue;
+    std::mutex mutex;
+    std::condition_variable cv;
+};
+
+class RawQueue {
+public:
+    void push(char c) {
+        const std::byte* ptr =
+            reinterpret_cast<const std::byte*>(&c);
+
+        for (std::size_t i = 0; i < sizeof(char); ++i)
+            bytes.push(ptr[i]);
+    }
+
+    void push(int n) {
+        const std::byte* ptr =
+            reinterpret_cast<const std::byte*>(&n);
+
+        for (std::size_t i = 0; i < sizeof(int); ++i)
+            bytes.push(ptr[i]);
+    }
+
+    char pop_char() {
+        return static_cast<char>(bytes.pop());
+    }
+
+    int pop_int() {
+        bytes.wait_until_size(sizeof(int));
+
+        int value{};
+        std::byte* ptr = reinterpret_cast<std::byte*>(&value);
+
+        for (std::size_t i = 0; i < sizeof(int); ++i)
+            ptr[i] = bytes.pop();
+
+        return value;
+    }
+
+private:
+    ConcurrentQueue<std::byte> bytes;
+};
+
+class DataArea {
+public: 
+    DataArea() : vec{}, st{-1} {
+    };
+
+    void set_value(int value, int idx) {
+        idx *= 4;
+        memcpy(&vec[idx], &value, sizeof(int));
+    }
+
+    void set_value(char value, int idx) {
+        vec[idx] = static_cast<std::byte>(value);
+    }
+
+    int load(int idx) {
+        int ans{};
+
+        memcpy(&ans, vec.data() + sizeof(int) * idx, sizeof(int));
+
+        return ans;
+    }
+
+    int pop_int() {
+        int ans{};
+
+        memcpy(&ans, vec.data() + (st - (sizeof(int) - 1)), sizeof(int));
+
+        st -= sizeof(int);
+        vec.resize(vec.size() - sizeof(int));
+
+        return ans;
+    }
+
+    char pop_char() {
+        char ans{static_cast<char>(vec[st])};
+
+        st -= sizeof(char);
+        vec.resize(vec.size() - sizeof(char));
+
+        return ans;
+    }
+
+    void push(int n) {
+        std::size_t oldSize{vec.size()};
+        vec.resize(oldSize + sizeof(int));
+
+        memcpy(vec.data() + oldSize, &n, sizeof(int));
+
+        st += sizeof(int);
+    }
+
+    void push(char c) {
+        std::size_t oldSize{vec.size()};
+        vec.resize(oldSize + sizeof(char));
+
+        memcpy(vec.data() + oldSize, &c, sizeof(char));
+
+        st += sizeof(char);
+    }
+
+    void reset() {
+        st = -1;
+    }
+
+    void alloc(int m) {
+        vec.resize(vec.size() + m * sizeof(int));
+        st += m * sizeof(int);
+
+#ifdef DEBUG
+    std::cerr << "\nst: " << st << "\n";
+#endif
+    }
+
+    void free(int m) {
+        vec.resize(vec.size() - m * sizeof(int));
+        st -= m * sizeof(int);
+
+#ifdef DEBUG
+    std::cerr << "\nst: " << st << "\n";
+#endif
+    }
+private:
+    std::vector<std::byte> vec;
+    int st;
+};
 
 // TODO: Maybe explicitly disable padding for this structure
 struct Command {
-    enum Type {
+    enum Type : std::uint8_t {
         CRCT, //1 arg
         CRVL, //1 arg
         ARMZ, //1 arg
@@ -49,7 +215,7 @@ struct Command {
         //Ops
         INPP, //0 arg
         AMEM, //1 arg
-        CDEM, //1 arg
+        DMEM, //1 arg
         PARA, //0 arg
     };
 
@@ -59,7 +225,7 @@ struct Command {
             case CRVL:
             case ARMZ:
             case AMEM:
-            case CDEM:
+            case DMEM:
             case DSVF:
             case DSVS:
                 return 1;
@@ -68,14 +234,66 @@ struct Command {
         };
     }
 
+    const char* to_string() const {
+        switch(type) {
+            case CRCT: return "CRCT";
+            case CRVL: return "CRVL";
+            case ARMZ: return "ARMZ";
+            case SOMA: return "SOMA";
+            case SUBT: return "SUBT";
+            case MULT: return "MULT";
+            case DIVI: return "DIVI";
+            case MODI: return "MODI";
+            case INVR: return "INVR";
+            case CONJ: return "CONJ";
+            case DISJ: return "DISJ";
+            case NEGA: return "NEGA";
+            case CMME: return "CMME";
+            case CMMA: return "CMMA";
+            case CMIG: return "CMIG";
+            case CMDG: return "CMDG";
+            case CMAG: return "CMAG";
+            case CMEG: return "CMEG";
+            case DSVS: return "DSVS";
+            case DSVF: return "DSVF";
+            case NOPE: return "NOPE";
+            case LEIT: return "LEIT";
+            case LECH: return "LECH";
+            case IMPR: return "IMPR";
+            case IMPC: return "IMPC";
+            case IMPE: return "IMPE";
+            case INPP: return "INPP";
+            case AMEM: return "AMEM";
+            case DMEM: return "DMEM";
+            case PARA: return "PARA";
+            default: return "UNK";
+        }
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const Command& cmd) {
+        out << cmd.to_string();
+
+        if (cmd.arg) {
+            out << " " << *cmd.arg;
+        }
+
+        return out;
+    }
+
     Type type;
     std::optional<int> arg;
 };
 
-// TODO: Refatorar a logica de armazenamento de dados, deve suportar int e char, operacoes push, pop, e set
 class Mepa {
 public:
-    Mepa(const std::filesystem::path& filename) : file(filename, std::ios::binary), data_area(100) {
+    enum class InputType {
+        Int,
+        Char,
+        None,
+        End,
+    };
+
+    Mepa(const std::filesystem::path& filename) : file(filename, std::ios::in | std::ios::binary), data_area{} {
         if (!file.is_open()) {
             throw std::runtime_error("Failed to read .lar file");
         }
@@ -106,56 +324,53 @@ public:
             throw std::runtime_error("Last command isn't a PARA");
         }
     }
+
     ~Mepa() {
         file.close();
     }
 
+    std::string pop_output() {
+        return output.pop();
+    }
+
+    void push_input(char c) {
+        input.push(c);
+    }
+
+    void push_input(int n) {
+        input.push(n);
+    }
+
+    InputType next_input_type() const {
+        if (pc >= code_area.size())
+            return InputType::End;
+
+        switch (code_area[pc].type) {
+            case Command::LEIT:
+                return InputType::Int;
+
+            case Command::LECH:
+                return InputType::Char;
+
+            default:
+                return InputType::None;
+        }
+    }
 
     void run() {
         while (pc < code_area.size()) {
-            handle_cmd();
 #ifdef DEBUG
             std::cerr << code_area[pc] << "\n";
 #endif
+            handle_cmd();
         }
+
+#ifdef DEBUG
+        std::cerr << "MEPA Program finished execution" << "\n";
+#endif
+        output.push("__EXIT__");
     }
 private:
-    int pop_int(int start) {
-        int ans{};
-
-        memcpy(&ans, data_area.data() + start, sizeof(int));
-
-        st -= sizeof(int);
-
-        return ans;
-    }
-
-    char pop_char(int start) {
-        char ans{static_cast<char>(data_area[start])};
-
-        st -= sizeof(char);
-
-        return ans;
-    }
-
-    void push_int(int n) {
-        std::size_t oldSize{data_area.size()};
-        data_area.resize(oldSize + sizeof(int));
-
-        memcpy(data_area.data() + oldSize, &n, sizeof(int));
-
-        st += sizeof(int);
-    }
-
-    void push_char(char c) {
-        std::size_t oldSize{data_area.size()};
-        data_area.resize(oldSize + sizeof(char));
-
-        memcpy(data_area.data() + oldSize, &c, sizeof(char));
-
-        st += sizeof(char);
-    }
-
     void handle_cmd() {
         if (pc < 0 || pc >= code_area.size()) {
 #ifdef DEBUG
@@ -164,108 +379,144 @@ private:
             return;
         }
 
+        int value{};
+        int value2{};
+        std::stringstream ss;
+
         Command cmd{code_area[pc]};
         switch(cmd.type) {
             case Command::CRCT:
-                push_int(*cmd.arg);
+                data_area.push(*cmd.arg);
                 ++pc;
                 break;
 
             case Command::CRVL:
-                int value{}
-                push_int(data_area[*cmd.arg]);
+                value = data_area.load(*cmd.arg);
+                data_area.push(value);
                 ++pc;
                 break;
 
             case Command::ARMZ:
-                data_area[*cmd.arg] = data_area[st];
+                value = data_area.pop_int();
+                data_area.set_value(value, *cmd.arg);
                 ++pc;
                 break;
 
             case Command::SOMA:
-                data_area[st-1] = data_area[st-1] + data_area[st];
+                value = data_area.pop_int();
+                value2 = data_area.pop_int();
+
+                data_area.push(value2 + value);
                 ++pc;
                 break;
 
             case Command::SUBT:
-                data_area[st-1] = data_area[st-1] - data_area[st];
-                --st;
+                value = data_area.pop_int();
+                value2 = data_area.pop_int();
+
+                data_area.push(value2 - value);
                 ++pc;
                 break;
 
             case Command::MULT:
-                data_area[st-1] = data_area[st-1] * data_area[st];
-                --st;
+                value = data_area.pop_int();
+                value2 = data_area.pop_int();
+
+                data_area.push(value2 * value);
                 ++pc;
                 break;
 
             case Command::DIVI:
-                data_area[st-1] = data_area[st-1] / data_area[st];
-                --st;
+                value = data_area.pop_int();
+                value2 = data_area.pop_int();
+
+                data_area.push(value2 / value);
                 ++pc;
                 break;
 
             case Command::MODI:
-                data_area[st-1] = data_area[st-1] % data_area[st];
-                --st;
+                value = data_area.pop_int();
+                value2 = data_area.pop_int();
+
+                data_area.push(value2 % value);
                 ++pc;
                 break;
 
             case Command::INVR:
-                data_area[st] = -data_area[st];
+                value = data_area.pop_int();
+
+                data_area.push(-value);
                 ++pc;
                 break;
 
             case Command::CONJ:
-                data_area[st-1] = data_area[st-1] && data_area[st];
-                --st;
+                value = data_area.pop_int();
+                value2 = data_area.pop_int();
+
+                data_area.push(value2 && value);
                 ++pc;
                 break;
 
             case Command::DISJ:
-                data_area[st-1] = data_area[st-1] || data_area[st];
-                --st;
+                value = data_area.pop_int();
+                value2 = data_area.pop_int();
+
+                data_area.push(value2 || value);
                 ++pc;
                 break;
 
             case Command::NEGA:
-                data_area[st] = !data_area[st];
+                value = data_area.pop_int();
+
+                data_area.push(!value);
                 ++pc;
                 break;
 
             case Command::CMME:
-                data_area[st-1] = (data_area[st-1] < data_area[st]) ? 1 : 0;
-                --st;
+                value = data_area.pop_int();
+                value2 = data_area.pop_int();
+
+                data_area.push((value2 < value) ? 1 : 0);
                 ++pc;
                 break;
 
             case Command::CMMA:
-                data_area[st-1] = (data_area[st-1] > data_area[st]) ? 1 : 0;
-                --st;
+                value = data_area.pop_int();
+                value2 = data_area.pop_int();
+
+                data_area.push((value2 > value) ? 1 : 0);
                 ++pc;
                 break;
 
             case Command::CMIG:
-                data_area[st-1] = (data_area[st-1] == data_area[st]) ? 1 : 0;
-                --st;
+                value = data_area.pop_int();
+                value2 = data_area.pop_int();
+
+                data_area.push((value2 == value) ? 1 : 0);
                 ++pc;
                 break;
 
             case Command::CMDG:
-                data_area[st-1] = (data_area[st-1] != data_area[st]) ? 1 : 0;
-                --st;
+                value = data_area.pop_int();
+                value2 = data_area.pop_int();
+
+                data_area.push((value2 != value) ? 1 : 0);
                 ++pc;
                 break;
 
             case Command::CMAG:
-                data_area[st-1] = (data_area[st-1] >= data_area[st]) ? 1 : 0;
-                --st;
+                value = data_area.pop_int();
+                value2 = data_area.pop_int();
+
+                data_area.push((value2 >= value) ? 1 : 0);
                 ++pc;
                 break;
 
             case Command::CMEG:
-                data_area[st-1] = (data_area[st-1] <= data_area[st]) ? 1 : 0;
-                --st;
+                value = data_area.pop_int();
+                value2 = data_area.pop_int();
+
+                data_area.push((value2 <= value) ? 1 : 0);
                 ++pc;
                 break;
 
@@ -274,18 +525,69 @@ private:
                 break;
 
             case Command::DSVF:
-                if (data_area[st] == 0) {
+                value = data_area.pop_int();
+
+                if (value == 0) {
                     pc = *cmd.arg;
                 } else {
                     ++pc;
                 }
-                --st;
+                break;
+
+            case Command::LEIT:
+                output.push("Enter a int: ");
+                data_area.push(input.pop_int());
+                ++pc;
+                break;
+
+            case Command::LECH:
+                output.push("Enter a char: ");
+                data_area.push(input.pop_char());
+                ++pc;
+                break;
+
+            case Command::IMPR:
+                ss << data_area.pop_int();
+
+                output.push(ss.str());
+                ++pc;
+                break;
+
+            case Command::IMPC:
+                ss << data_area.pop_char();
+
+                output.push(ss.str());
+                ++pc;
+                break;
+            
+            case Command::IMPE:
+                ss << data_area.pop_int() << "\n";
+
+                output.push(ss.str());
+                ++pc;
+                break;
+
+            case Command::INPP:
+                data_area.reset();
+                ++pc;
+                break;
+
+            case Command::AMEM:
+                data_area.alloc(*cmd.arg);
+                ++pc;
+                break;
+
+            case Command::DMEM:
+                data_area.free(*cmd.arg);
+                ++pc;
+                break;
+
+            case Command::PARA:
+                ++pc;
                 break;
 
             case Command::NOPE:
-#ifdef DEBUG
-                std::cerr << "NOPE\n";
-#endif
+                ++pc;
                 break;
             default:
                 break;
@@ -293,10 +595,12 @@ private:
     }
 
     int pc{}; // Program Counter
-    int st{-1}; // Stack Top
 
     std::fstream file;
 
     std::vector<Command> code_area;
-    std::vector<std::byte> data_area;
+    DataArea data_area;
+
+    RawQueue input;
+    ConcurrentQueue<std::string> output;
 };
--- packages/core-lib/src/main.cpp
+++ packages/core-lib/src/main.cpp
@@ -1,67 +1,281 @@
 #include "compilador.h"
 #include <iostream>
 #include <filesystem>
+#include <thread>
 #include "util.h"
-#include "analisador_sintatico_procedimento.h"
+
+#define NDEBUG
+#include "mepa.h"
+
+template<typename T>
+void writeBinary(std::ostream& os, const T& value) {
+    os.write(reinterpret_cast<const char*>(&value), sizeof(T));
+}
+
+void background_output(Mepa& mepa) {
+    while (true) {
+        std::string s = mepa.pop_output();
+
+        if (s == "__EXIT__")
+            break;
+
+        std::cout << s << std::flush;
+    }
+}
+
+void background_input(Mepa& mepa) {
+    while (true) {
+        switch (mepa.next_input_type()) {
+            case Mepa::InputType::Int: {
+                int value;
+                std::cin >> value;
+                mepa.push_input(value);
+                break;
+            }
+
+            case Mepa::InputType::Char: {
+                char value;
+                std::cin >> value;
+                mepa.push_input(value);
+                break;
+            }
+        
+            case Mepa::InputType::End: return;
+        }
+    }
+}
+
+void generate_mepa_example1(const std::filesystem::path& filename) {
+    std::fstream file(filename, std::ios::binary | std::ios::out);
+
+    writeBinary(file, Command::INPP);
+
+    writeBinary(file, Command::AMEM);
+    writeBinary(file, 1);
+
+    writeBinary(file, Command::AMEM);
+    writeBinary(file, 1);
+
+    writeBinary(file, Command::LEIT);
+
+    writeBinary(file, Command::ARMZ);
+    writeBinary(file, 0);
+
+    writeBinary(file, Command::LEIT);
+
+    writeBinary(file, Command::ARMZ);
+    writeBinary(file, 1);
+
+    writeBinary(file, Command::CRVL);
+    writeBinary(file, 0);
+
+    writeBinary(file, Command::CRVL);
+    writeBinary(file, 1);
+
+    writeBinary(file, Command::CMMA);
+
+    writeBinary(file, Command::DSVF);
+    writeBinary(file, 0);
+
+    writeBinary(file, Command::CRVL);
+    writeBinary(file, 0);
+
+    writeBinary(file, Command::IMPE);
+    writeBinary(file, Command::NOPE);
+    writeBinary(file, Command::PARA);
+
+    file.close();
+}
+
+void generate_mepa_example2(const std::filesystem::path& filename) {
+    std::fstream file(filename, std::ios::binary | std::ios::out);
+
+    writeBinary(file, Command::INPP);
+
+    writeBinary(file, Command::AMEM);
+    writeBinary(file, 1);
+
+    writeBinary(file, Command::AMEM);
+    writeBinary(file, 1);
+
+    writeBinary(file, Command::AMEM);
+    writeBinary(file, 1);
+
+    writeBinary(file, Command::LEIT);
+
+    writeBinary(file, Command::ARMZ);
+    writeBinary(file, 1);
+
+    writeBinary(file, Command::LEIT);
+
+    writeBinary(file, Command::ARMZ);
+    writeBinary(file, 2);
+
+    writeBinary(file, Command::CRVL);
+    writeBinary(file, 0);
+
+    writeBinary(file, Command::CRVL);
+    writeBinary(file, 2);
+
+    writeBinary(file, Command::CRCT);
+    writeBinary(file, 10);
+
+    writeBinary(file, Command::CRVL);
+    writeBinary(file, 1);
+
+    writeBinary(file, Command::DIVI);
+    
+    writeBinary(file, Command::CRCT);
+    writeBinary(file, 1);
+
+    writeBinary(file, Command::SOMA);
+    writeBinary(file, Command::MULT);
+    writeBinary(file, Command::SOMA);
+
+    writeBinary(file, Command::ARMZ);
+    writeBinary(file, 0);
+
+    writeBinary(file, Command::CRVL);
+    writeBinary(file, 0);
+
+    writeBinary(file, Command::IMPE);
+    
+    writeBinary(file, Command::CRVL);
+    writeBinary(file, 1);
+
+    writeBinary(file, Command::IMPE);
+
+    writeBinary(file, Command::CRVL);
+    writeBinary(file, 2);
+
+    writeBinary(file, Command::IMPE);
+   
+    writeBinary(file, Command::CRVL);
+    writeBinary(file, 0);
+
+    writeBinary(file, Command::CRCT);
+    writeBinary(file, 0);
+
+    writeBinary(file, Command::CMMA);
+
+    writeBinary(file, Command::DSVF);
+    writeBinary(file, 44);
+
+    writeBinary(file, Command::CRVL);
+    writeBinary(file, 1);
+
+    writeBinary(file, Command::CRCT);
+    writeBinary(file, 0);
+
+    writeBinary(file, Command::CMMA);
+
+    writeBinary(file, Command::DSVF);
+    writeBinary(file, 37);
+
+    writeBinary(file, Command::CRVL);
+    writeBinary(file, 0);
+
+    writeBinary(file, Command::CRVL);
+    writeBinary(file, 1);
+
+    writeBinary(file, Command::MULT);
+
+    writeBinary(file, Command::ARMZ);
+    writeBinary(file, 1);
+
+    writeBinary(file, Command::DSVS);
+    writeBinary(file, 42);
+
+    writeBinary(file, Command::NOPE);
+    
+    writeBinary(file, Command::CRVL);
+    writeBinary(file, 1);
+
+    writeBinary(file, Command::CRVL);
+    writeBinary(file, 0);
+
+    writeBinary(file, Command::SOMA);
+    
+    writeBinary(file, Command::ARMZ);
+    writeBinary(file, 1);
+
+    writeBinary(file, Command::NOPE);
+
+    writeBinary(file, Command::DSVS);
+    writeBinary(file, 60);
+
+    writeBinary(file, Command::NOPE);
+
+    writeBinary(file, Command::CRVL);
+    writeBinary(file, 1);
+
+    writeBinary(file, Command::CRCT);
+    writeBinary(file, 0);
+
+    writeBinary(file, Command::CMMA);
+
+    writeBinary(file, Command::DSVF);
+    writeBinary(file, 54);
+
+    writeBinary(file, Command::CRVL);
+    writeBinary(file, 1);
+
+    writeBinary(file, Command::CRCT);
+    writeBinary(file, -1);
+
+    writeBinary(file, Command::MULT);
+
+    writeBinary(file, Command::ARMZ);
+    writeBinary(file, 1);
+
+    writeBinary(file, Command::DSVS);
+    writeBinary(file, 59);
+
+    writeBinary(file, Command::NOPE);
+
+    writeBinary(file, Command::CRVL);
+    writeBinary(file, 1);
+
+    writeBinary(file, Command::CRVL);
+    writeBinary(file, 2);
+
+    writeBinary(file, Command::SOMA);
+
+    writeBinary(file, Command::ARMZ);
+    writeBinary(file, 1);
+
+    writeBinary(file, Command::NOPE);
+    writeBinary(file, Command::NOPE);
+
+    writeBinary(file, Command::PARA);
+
+    file.close();
+}
+
+void execute_generate_mepa_example(const std::filesystem::path& filename) {
+    try {
+        Mepa mepa(filename);
+
+        std::thread output_thread(background_output, std::ref(mepa));
+        std::thread input_thread(background_input, std::ref(mepa));
+
+        mepa.run();
+
+        input_thread.join();
+        output_thread.join();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << "\n";
+    }
+}
 
 int main() {
-    std::string teste = parse_file_to_string(std::filesystem::path(EXAMPLES_DIR) / "correto1.txt");
-    /*
-    std::string teste = "(1 + 1) / 3 * 9\n   \n\n\n\n   p";
+    auto filepath1{std::filesystem::path(EXAMPLES_DIR) / "mepa_example_1.lar"};
+    auto filepath2{std::filesystem::path(EXAMPLES_DIR) / "mepa_example_2.lar"};
 
-    LexicalAnalysisCalc test(teste);
 
-    test.analyze();
-
-    const std::vector<TokenCalc> tokens = test.get_tokens();
-
-    print_tokens_calc(tokens);
-    */
-
-    SyntacticAnalyzerProcedures analysis(teste);
-
-    analysis.run();
-
-    auto snapshots = analysis.get_snapshots();
-
-    int step = 1;
-    for (const auto& s : snapshots) {
-        std::cout << "================================================\n";
-        std::cout << " PASSO " << step++ << ": " << s.action << "\n";
-        std::cout << "================================================\n";
-
-        // --- SESSÃO DE ERROS ---
-        std::cout << "[ ERROS ]\n";
-        auto q = s.curr_errors; // Copia por valor para poder consumir
-        if (q.empty()) {
-            std::cout << "  (Nenhum erro)\n";
-        } else {
-            while (!q.empty()) {
-                // Se a sua struct tiver linha e coluna, você pode adicionar aqui:
-                // std::cout << "  - [L:" << q.front().line << ", C:" << q.front().col << "] ";
-                std::cout << "  - " << q.front().error << "\n";
-                q.pop(); // <- CORREÇÃO DO BUG DO LOOP INFINITO
-            }
-        }
-        std::cout << "\n";
-
-        // --- SESSÃO DA PILHA DE SÍMBOLOS ---
-        std::cout << "[ PILHA DE SÍMBOLOS (Topo -> Base) ]\n  ";
-        auto stack = s.curr_symbols; // Copia por valor para poder consumir
-        if (stack.empty()) {
-            std::cout << "(Vazia)";
-        } else {
-            bool first = true;
-            while (!stack.empty()) {
-                if (!first) std::cout << " | ";
-                std::cout << stack.top().name;
-                stack.pop();
-                first = false;
-            }
-        }
-        std::cout << "\n\n";
-    }
-    analysis.print_current_lexeme();
+    generate_mepa_example2(filepath2);
+    //generate_mepa_example1(filepath1);
+    execute_generate_mepa_example(filepath2);
 
     return 0;
 }
